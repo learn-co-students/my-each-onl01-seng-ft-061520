1dd9a851eff4f14ebb48837da683dd3e0992b909
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-20 18:19:34 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collections) # put argument(s) here
+  # code heredef my_each(collections)
+  i = 0
+  while i < collections.length
+  yield (collections[i])
+  i += 1
+end
+collections
 end
